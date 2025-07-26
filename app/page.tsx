e701216a9929ae33da248a78fb35bf305d0fba1e
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2025-07-25 22:43:05 -0500

    Microsoft update

--- app/page.tsx
+++ app/page.tsx
@@ -22,8 +22,13 @@ export default function Page() {
         </div>
       </div>
       <p className="mb-4">
-        I am a solo AI engineer at the <a href="https://getnerdio.com/press/nerdio-secures-series-c-investment-from-general-atlantic/" className="underline">unicorn</a> startup, <a href="https://getnerdio.com" target="_blank" rel="noopener noreferrer" className="underline">Nerdio</a>. 
-        I have worked on AssistPro (<a href="https://www.forbes.com/sites/adrianbridgwater/2024/04/02/the-rise-of-infused-lower-level-ai/" rel="noopener noreferrer" className="underline">mentioned in Forbes</a>), AnalyticsPro (Kusto Query Language code generator tool), <a href="https://nmehelp.getnerdio.com/hc/en-us/articles/30352365570957-Manage-Nerdio-Manager-Copilot" className="underline">Nerdio Copilot</a>, which has led to Nerdio landing on <a href="https://www.crn.com/news/cloud/2024/the-20-hottest-ai-cloud-companies-the-2024-crn-ai-100?page=14" className="underline">CRN's 2024 Top 100 AI list</a>.
+        I am a software engineer at Microsoft, working on planet-scale distributed systems in the Azure Storage team. 
+        I help to provide limitless data storage to Microsoft's most demanding customers. 
+        I also work on AI agents, trying to drive developer productivity.
+      </p>
+      <p className="mb-4">
+        Previously, I was a solo AI engineer at the <a href="https://getnerdio.com/press/nerdio-secures-series-c-investment-from-general-atlantic/" className="underline">unicorn</a> startup, <a href="https://getnerdio.com" target="_blank" rel="noopener noreferrer" className="underline">Nerdio</a>. 
+        I worked on AssistPro (<a href="https://www.forbes.com/sites/adrianbridgwater/2024/04/02/the-rise-of-infused-lower-level-ai/" rel="noopener noreferrer" className="underline">mentioned in Forbes</a>), AnalyticsPro (Kusto Query Language code generator tool), <a href="https://nmehelp.getnerdio.com/hc/en-us/articles/30352365570957-Manage-Nerdio-Manager-Copilot" className="underline">Nerdio Copilot</a>, which has led to Nerdio landing on CRN's Top AI 100 list for <a href="https://www.crn.com/news/cloud/2024/the-20-hottest-ai-cloud-companies-the-2024-crn-ai-100?page=14" className="underline">2024</a> and <a href="https://www.crn.com/news/cloud/2025/the-20-hottest-ai-cloud-companies-the-2025-crn-ai-100?page=15" className="underline">2025</a>.
       </p>
       <p className="mb-4">
         My earlier research on low-resource NLP earned the first spot at ACL/COLING workshops; details live on my <a href="https://scholar.google.com/citations?user=TSl2LFMAAAAJ&hl=en" className="underline">Google Scholar</a>.
